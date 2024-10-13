ae00ace52860a78c8594bcadddc96c99e8a622ea
Miguel Angel Hernandez Corona <miguelhc@MacBook-Air-de-Miguel.local>
2024-10-12 20:35:53 -0600

    first committ

---
+++ server/index.js
@@ -0,0 +1,93 @@
+// index.js
+
+import express from 'express';
+import logger from 'morgan';
+import path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { Server } from 'socket.io';
+import { createServer } from 'node:http';
+import DBConnector from './dbconnector.js';
+import cors from 'cors';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+(async () => {
+    try {
+        await DBConnector.query('CREATE TABLE IF NOT EXISTS messages (id INT AUTO_INCREMENT PRIMARY KEY, content TEXT, user TEXT)');
+    } catch (e) {
+        console.error('Error creating table:', e);
+    }
+
+    const port = process.env.PORT ?? 4040;
+    const app = express();
+    
+    // Configuración de CORS
+    const allowedOrigins = [
+        "https://clientesocketweb.onrender.com",
+        "https://9235-2806-104e-3-97c6-649c-1891-e11d-3cfc.ngrok-free.app" // Añadir la URL de ngrok
+    ];
+
+    app.use(cors({
+        origin: allowedOrigins,
+        methods: ["GET", "POST"],
+        credentials: true
+    }));
+
+    const server = createServer(app);
+    const io = new Server(server, {
+        cors: {
+            origin: allowedOrigins,
+            methods: ["GET", "POST"],
+            allowedHeaders: ["my-custom-header"],
+            credentials: true
+        },
+        connectionStateRecovery: {}
+    });
+
+    io.on('connection', (socket) => {
+        console.log('A user connected!');
+
+        socket.on('disconnect', () => {
+            console.log('A user has disconnected');
+        });
+
+        socket.on('chat message', async (msg) => {
+            const user = socket.handshake.auth.username ?? 'Anónimo';
+
+            try {
+                await DBConnector.query('INSERT INTO messages (content, user) VALUES (?, ?)', [msg, user]);
+                console.log(`Message: ${msg} from: ${user}`);
+
+                const [latestMessage] = await DBConnector.query('SELECT * FROM messages ORDER BY id DESC LIMIT 1');
+                io.emit('chat message', msg, latestMessage.id, user);
+            } catch (e) {
+                console.error('Error handling chat message:', e);
+            }
+        });
+
+        if (!socket.recovered) {
+            (async () => {
+                try {
+                    const serverOffset = socket.handshake.auth.serverOffset ?? 0;
+                    const results = await DBConnector.query('SELECT id, content, user FROM messages WHERE id > ?', [serverOffset]);
+                    results.forEach(result => {
+                        socket.emit('chat message', result.content, result.id, result.user);
+                    });
+                } catch (e) {
+                    console.error('Error sending previous messages:', e);
+                }
+            })();
+        }
+    });
+
+    app.use(logger('dev'));
+
+    // app.get('/', (req, res) => {
+    //     res.sendFile(path.join(__dirname, 'client', 'index.html'));
+    // });
+
+    server.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+})();
